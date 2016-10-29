e0ae21b22c4a2726224da55c2573c4074868a89a
jainsukriti <sukriti.10992@gmail.com>
2016-10-29 20:42:10 +0530

    Update TestingLruCache.scala

--- CacheProject/src/test/scala/testers/TestingLruCache.scala
+++ CacheProject/src/test/scala/testers/TestingLruCache.scala
@@ -5,7 +5,7 @@ import swappingSchemes.LruCache
 
 class TestingLruCache extends WordSpec {
   val testCache = new LruCache [String, Long]//create cache
-  //fill cache
+  //fill cache with 5 strings
   testCache.insertPair("string1", 1)
   testCache.insertPair("string2", 2)
   testCache.insertPair("string3", 3)
@@ -16,7 +16,7 @@ class TestingLruCache extends WordSpec {
     "cached" should {
       assert(testCache.getValueIfExists("string1").isDefined)
       assert(testCache.getValueIfExists("string3").isDefined)
-      println("All tests for cached keys pass")
+      println("All tests for keys that are cached pass")
     }
   }
   "not cached" should {
@@ -24,7 +24,7 @@ class TestingLruCache extends WordSpec {
     testCache.insertPair("string7", 7)
     assert(testCache.getValueIfExists("string2").isEmpty)
     assert(testCache.getValueIfExists("string4").isEmpty)
-    println("All tests for keys not cached pass")
+    println("All tests for keys that are not cached pass")
   }
-  println("All tests for Lru pass")
+  println("All tests for LRU pass")
 }
